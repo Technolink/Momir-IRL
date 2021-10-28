f9b61b073a5cb014cb102bb733ad0e0b9a7545fc
Alan Goldman <mrauxy@gmail.com>
2021-10-27 22:16:43 -0400

    Initial commit

---
+++ bitmap_print_test/bitmap_print_test.ino
@@ -0,0 +1,48 @@
+/*------------------------------------------------------------------------
+  Example sketch for Adafruit Thermal Printer library for Arduino.
+  Demonstrates a few text styles & layouts, bitmap printing, etc.
+
+  IMPORTANT: DECLARATIONS DIFFER FROM PRIOR VERSIONS OF THIS LIBRARY.
+  This is to support newer & more board types, especially ones that don't
+  support SoftwareSerial (e.g. Arduino Due).  You can pass any Stream
+  (e.g. Serial1) to the printer constructor.  See notes below.
+
+  You may need to edit the PRINTER_FIRMWARE value in Adafruit_Thermal.h
+  to match your printer (hold feed button on powerup for test page).
+  ------------------------------------------------------------------------*/
+
+#include "Adafruit_Thermal.h"
+//#include "adalogo.h"
+//#include "monochrome.h"
+#include "test.h"
+
+// Here's the new syntax when using SoftwareSerial (e.g. Arduino Uno) ----
+// If using hardware serial instead, comment out or remove these lines:
+
+#include "SoftwareSerial.h"
+#define TX_PIN 6 // Arduino transmit  YELLOW WIRE  labeled RX on printer
+#define RX_PIN 5 // Arduino receive   GREEN WIRE   labeled TX on printer
+
+SoftwareSerial mySerial(RX_PIN, TX_PIN); // Declare SoftwareSerial obj first
+Adafruit_Thermal printer(&mySerial);     // Pass addr to printer constructor
+// Then see setup() function regarding serial & printer begin() calls.
+
+// Here's the syntax for hardware serial (e.g. Arduino Due) --------------
+// Un-comment the following line if using hardware serial:
+
+//Adafruit_Thermal printer(&Serial1);      // Or Serial2, Serial3, etc.
+
+// -----------------------------------------------------------------------
+
+void setup() {
+  mySerial.begin(19200);  // Initialize SoftwareSerial
+  printer.begin();        // Init printer (same regardless of serial type)
+
+  printer.printBitmap(monochrome_width, monochrome_height, monochrome_data);
+  printer.feed(4);
+ 
+  //printer.setDefault(); // Restore printer to defaults
+}
+
+void loop() {
+}
