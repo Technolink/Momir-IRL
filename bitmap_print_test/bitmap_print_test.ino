36e0d9c0cef61ea8e0cb18f2514ad00611c11d95
Alan Goldman <mrauxy@gmail.com>
2021-10-28 00:30:19 -0400

    Basic bluetooth!

--- bitmap_print_test/bitmap_print_test.ino
+++ bitmap_print_test/bitmap_print_test.ino
@@ -1,45 +1,53 @@
-/*------------------------------------------------------------------------
-  Example sketch for Adafruit Thermal Printer library for Arduino.
-  Demonstrates a few text styles & layouts, bitmap printing, etc.
-
-  IMPORTANT: DECLARATIONS DIFFER FROM PRIOR VERSIONS OF THIS LIBRARY.
-  This is to support newer & more board types, especially ones that don't
-  support SoftwareSerial (e.g. Arduino Due).  You can pass any Stream
-  (e.g. Serial1) to the printer constructor.  See notes below.
-
-  You may need to edit the PRINTER_FIRMWARE value in Adafruit_Thermal.h
-  to match your printer (hold feed button on powerup for test page).
-  ------------------------------------------------------------------------*/
-
 #include "Adafruit_Thermal.h"
 //#include "adalogo.h"
-#include "monochrome.h"
+//#include "monochrome.h"
 //#include "scarabgod.h"
 
-// Here's the new syntax when using SoftwareSerial (e.g. Arduino Uno) ----
-// If using hardware serial instead, comment out or remove these lines:
-
 #include "SoftwareSerial.h"
-#define TX_PIN  6  // Arduino transmit  YELLOW WIRE   labeled RX  on printer
-#define RX_PIN  5  // Arduino receive   GREEN  WIRE   labeled TX  on printer
+#define LED_PIN         13 // debug led         GREEN  WIRE TO BOARD
+#define PRINTER_TX_PIN  6  // Arduino transmit  YELLOW WIRE   labeled RX  on printer
+#define PRINTER_RX_PIN  5  // Arduino receive   GREEN  WIRE   labeled TX  on printer
 // https://learn.adafruit.com/mini-thermal-receipt-printer/hacking
-#define DTR_PIN 4 // Arduino DTR       ORANGE WIRE   labeled DTR on printer
+#define PRINTER_DTR_PIN 4  // Arduino DTR       ORANGE WIRE   labeled DTR on printer
 
-SoftwareSerial mySerial(RX_PIN, TX_PIN); // Declare SoftwareSerial obj first
-Adafruit_Thermal printer(&mySerial, DTR_PIN);     // Pass addr to printer constructor
+#define BLUETHT_TX_PIN  3  // Arduino transmit  BLUE   WIRE   labeled RX  on bluetooth
+#define BLUETHT_RX_PIN  2  // Arduino receive   PURPLE WIRE   labeled TX  on bluetooth
+
+SoftwareSerial PrinterSerial(PRINTER_RX_PIN, PRINTER_TX_PIN); // Declare SoftwareSerial obj first
+Adafruit_Thermal Printer(&PrinterSerial, PRINTER_DTR_PIN);     // Pass addr to printer constructor
+
+SoftwareSerial BluetoothSerial(BLUETHT_RX_PIN, BLUETHT_TX_PIN); // Declare SoftwareSerial obj first
 
 void setup() {
-  mySerial.begin(19200);  // Initialize SoftwareSerial
-  printer.begin();        // Init printer (same regardless of serial type)
+  PrinterSerial.begin(19200);
+  Printer.begin();
 
-  printer.setHeatConfig(4, 255, 255);
+  Printer.setHeatConfig(4, 255, 255);
   //printer.setTimes(30000, 2100);
   
-  printer.printBitmap(monochrome_width, monochrome_height, monochrome_data);
-  printer.feed(4);
- 
+  //printer.printBitmap(monochrome_width, monochrome_height, monochrome_data);
+  //printer.feed(4);
   //printer.setDefault(); // Restore printer to defaults
+
+  BluetoothSerial.begin(9600); 
+
+  Serial.begin(9600);
 }
 
 void loop() {
+  int flag = 0;
+  if (BluetoothSerial.available()) {
+    flag = BluetoothSerial.read();
+  }
+  if (flag == '1') {
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("LED on");
+    BluetoothSerial.println("LED on");
+    flag = 0;
+  } else if (flag == '0') {
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("LED off");
+    BluetoothSerial.println("LED off");
+    flag = 0;
+  }
 }
